19a3408b431d5c03a3b012216893fe9b0e8347ae
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-18 12:25:07 +0200

    classes defined

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -3,13 +3,33 @@
 # Card should be represented as string in this format:
 # 9 Hearts
 # Jack Diamonds
+
 # Create a Deck class, that has a list of cards in it
 # Create a constructor that takes a whole number as parameter
 # The constructor should fill the list with the number of different cards using at least 4 different colors (except if the number given is smaller than four, than all cards should have different colors)
+
 # We should be able to shuffle the deck, which randomly orders the cards
+
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
+
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+
+class Card():
+    def __init__(self, value, color):
+        self.value = value
+        self.color = color
+
+class Deck():
+    card_list = []
+    def __init__(self, n):
+        self.n = n
+
+    def draw():
+
+
+
+
 deck = Deck(12)
 print(deck)
 # Should print out:
