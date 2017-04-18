18751fde7e3907ae0b7686d7f7e71db2cf38af5a
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-18 11:25:22 +0200

    feat. copying files

--- copy/copy.py
+++ copy/copy.py
@@ -15,5 +15,22 @@ def copy():
         print('copy [source] [destination]')
     elif len(controller) == 2:
         print('No destination provided')
+    elif len(controller) == 3:
+        try:
+            read()
+        except FileNotFoundError:
+            print('One or both of your files are not found')
+#
+def read():
+    controller = sys.argv[1:]
+    file_1 = str(controller[1])
+    source = open(file_1,'r')
+    source = source.read()
+
+    controller = sys.argv[1:]
+    file_2 = str(controller[2])
+    dest = open(file_2,'a')
+    dest.write(source)
 
 copy()
+#read_write()
