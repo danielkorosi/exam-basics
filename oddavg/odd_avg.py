177fc54390c5c570d1f710f78985e581967bc6d1
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-18 10:03:34 +0200

    class created, tests created for odd_average method

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,13 +1,15 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+class OddAvg():
+    def odd_average(self, num_list=[]):
+        self.odd_list = []
+        for i in num_list:
+            if i % 2 != 0:
+                self.odd_list.append(i)
+        average = sum(self.odd_list)/(len(self.odd_list))
+        return average
 
-def odd_average(num_list=[]):
-    odd_list = []
-    for i in num_list:
-        if i % 2 != 0:
-            odd_list.append(i)
-    average = sum(odd_list)/(len(odd_list)+1)
-    return average
+example_list = OddAvg()
 
-print(odd_average([2,3,7,13,6]))
+print(example_list.odd_average([2,3,5,0,6,7,1]))
---
+++ oddavg/test_odd_avg.py
@@ -0,0 +1,16 @@
+import unittest
+from odd_avg import OddAvg
+
+class TestOddAverage(unittest.TestCase):
+    def test_general(self):
+        test_list = OddAvg()
+        self.assertEqual(test_list.odd_average([2,4,1,9]), 5)
+    def test_for_zero(self):
+        test_list = OddAvg()
+        self.assertEqual(test_list.odd_average([0,1,4]), 1)
+    def test_for_odd_selection(self):
+        test_list = OddAvg()
+        self.assertEqual(test_list.odd_average([7,2,4,6,8]), 7)
+
+if __name__ == "__main__":
+    unittest.main()
